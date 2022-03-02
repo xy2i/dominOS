bedc7e02f3ec9668347cbe3e409a90a0682849bc
Arthur Sarry <arthur.sarry@gmail.com>
2022-03-02 12:39:19 +0100

    test-kernel: Add a test testing the use of args when starting a func

---
+++ kernel/test-kernel/test_start_with_args.h
@@ -0,0 +1,11 @@
+#ifndef __TEST_START_WITH_ARGS_H__
+#define __TEST_START_WITH_ARGS_H__
+
+struct point {
+    int x;
+    int y;
+};
+
+void test_start_with_args_main(void *args);
+
+#endif //__TEST_START_WITH_ARGS_H__
