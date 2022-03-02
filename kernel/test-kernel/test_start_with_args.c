8cb7e23dd75896c488d95a06ec8e75afcb7b616e
Arthur Sarry <arthur.sarry@gmail.com>
2022-03-02 12:39:19 +0100

    test-kernel: Add a test testing the use of args when starting a func

---
+++ kernel/test-kernel/test_start_with_args.c
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * Test Start With Args
+ *
+ * Tests the passing of args to when starting a new process.
+ ******************************************************************************/
+
+#include "stdio.h"
+#include "test_start_with_args.h"
+
+void test_start_with_args_main(void *args)
+{
+    struct point *p = (struct point *) args;
+    printf("x: %i y: %i\n", p->x, p->y);
+}
