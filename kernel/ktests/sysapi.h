aabd22abc20175838269aebf27bad3342a35fe64
Arthur Sarry <arthur.sarry@gmail.com>
2022-03-16 12:57:40 +0100

    ktests: add test11

--- kernel/ktests/sysapi.h
+++ kernel/ktests/sysapi.h
@@ -8,7 +8,6 @@
 
 /* it.c */
 void test_it(void);
-
 /* math.h */
 typedef unsigned long long uint_fast64_t;
 typedef unsigned long uint_fast32_t;
@@ -17,5 +16,16 @@ void setSeed(uint_fast64_t _s);
 unsigned long rand();
 unsigned long long div64(unsigned long long num, unsigned long long div,
 			 unsigned long long *rem);
+
+/* Wrapper sur les verrous basés sur les sémaphores ou les files de messages */
+union sem {
+    int fid;
+    int sem;
+};
+
+void xwait(union sem *s);
+void xsignal(union sem *s);
+void xsdelete(union sem *s);
+void xscreate(union sem *s);
 
 #endif
