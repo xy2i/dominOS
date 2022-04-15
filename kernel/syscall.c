64552954a5865996da41862f412713ce2d493983
Arthur Sarry <arthur.sarry@gmail.com>
2022-04-15 17:49:59 +0200

    syscall: Removing the extern declaration of syscall_1

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,8 +1,8 @@
 #include "syscall.h"
 #include "stdio.h"
+#include "syscall_asm.h"
 
 // syscall handlers are in syscall.h
-extern void syscall_1(int syscall_number, void *arg1);
 #define SYSCALL_1(syscall_number, arg1) syscall_1(syscall_number, (void *)arg1)
 
 int ggetprio(int pid)
