a0f59d23d094161ede67ef2e1d3f1eff2790d09f
Arthur Sarry <arthur.sarry@gmail.com>
2022-04-15 17:46:30 +0200

    syscall: Calling syscall_1.

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -7,7 +7,6 @@ extern void syscall_1(int syscall_number, void *arg1);
 
 int ggetprio(int pid)
 {
-    printf("%d", pid);
-    //syscall_1(2, (void *)pid);
+    syscall_1(2, (void *)pid);
     return 0;
 }
