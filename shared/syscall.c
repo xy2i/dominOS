813d2878901faf55d7eb407a9327b4c16993c175
Arthur Sarry <arthur.sarry@gmail.com>
2022-04-15 10:10:48 +0200

    syscall: Coding the syscall_x directly in assembly due to an unknown gcc implied error.

--- shared/syscall.c
+++ shared/syscall.c
@@ -1,71 +1,13 @@
-
 #include "syscall.h"
+#include "stdio.h"
 
-#define syscall_0(syscall_number, arg1)                                        \
-    ({                                                                         \
-        __asm__ __volatile__("movl %%eax,%0" ::"a"(syscall_number));           \
-        __asm__ __volatile__("int $49");                                       \
-    })
+// syscall handlers are in syscall.h
+extern void syscall_1(int syscall_number, void *arg1);
+#define SYSCALL_1(syscall_number, arg1) syscall_1(syscall_number, (void *)arg1)
 
-#define syscall_1(syscall_number, arg1)                                        \
-    ({                                                                         \
-        __asm__ __volatile__("movl %%eax,%0" ::"a"(syscall_number));           \
-        __asm__ __volatile__("movl %%ebx,%0" ::"a"(arg1));                     \
-        __asm__ __volatile__("int $49");                                       \
-    })
-
-#define syscall_2(syscall_number, arg1, arg2)                                  \
-    ({                                                                         \
-        __asm__ __volatile__("movl %%eax,%0" ::"a"(syscall_number));           \
-        __asm__ __volatile__("movl %%ebx,%0" ::"a"(arg1));                     \
-        __asm__ __volatile__("movl %%ecx,%0" ::"a"(arg2));                     \
-        __asm__ __volatile__("int $49");                                       \
-    })
-
-#define syscall_3(syscall_number, arg1, arg2, arg3)                            \
-    ({                                                                         \
-        __asm__ __volatile__("movl %%eax,%0" ::"a"(syscall_number));           \
-        __asm__ __volatile__("movl %%ebx,%0" ::"a"(arg1));                     \
-        __asm__ __volatile__("movl %%ecx,%0" ::"a"(arg2));                     \
-        __asm__ __volatile__("movl %%edx,%0" ::"a"(arg3));                     \
-        __asm__ __volatile__("int $49");                                       \
-    })
-
-#define syscall_4(syscall_number, arg1, arg2, arg3, arg4)                      \
-    ({                                                                         \
-        __asm__ __volatile__("movl %%eax,%0" ::"a"(syscall_number));           \
-        __asm__ __volatile__("movl %%ebx,%0" ::"a"(arg1));                     \
-        __asm__ __volatile__("movl %%ecx,%0" ::"a"(arg2));                     \
-        __asm__ __volatile__("movl %%edx,%0" ::"a"(arg3));                     \
-        __asm__ __volatile__("movl %%esi,%0" ::"a"(arg4));                     \
-        __asm__ __volatile__("int $49");                                       \
-    })
-
-#define syscall_5(syscall_number, arg1, arg2, arg3, arg4, arg5)                \
-    ({                                                                         \
-        __asm__ __volatile__("movl %%eax,%0" ::"a"(syscall_number));           \
-        __asm__ __volatile__("movl %%ebx,%0" ::"a"(arg1));                     \
-        __asm__ __volatile__("movl %%ecx,%0" ::"a"(arg2));                     \
-        __asm__ __volatile__("movl %%edx,%0" ::"a"(arg3));                     \
-        __asm__ __volatile__("movl %%esi,%0" ::"a"(arg4));                     \
-        __asm__ __volatile__("movl %%edi,%0" ::"a"(arg5));                     \
-        __asm__ __volatile__("int $49");                                       \
-    })
-
-#define syscall_6(syscall_number, arg1, arg2, arg3, arg4, arg5, arg6)          \
-    ({                                                                         \
-        __asm__ __volatile__("movl %%eax,%0" ::"a"(syscall_number));           \
-        __asm__ __volatile__("movl %%ebx,%0" ::"a"(arg1));                     \
-        __asm__ __volatile__("movl %%ecx,%0" ::"a"(arg2));                     \
-        __asm__ __volatile__("movl %%edx,%0" ::"a"(arg3));                     \
-        __asm__ __volatile__("movl %%esi,%0" ::"a"(arg4));                     \
-        __asm__ __volatile__("movl %%edi,%0" ::"a"(arg5));                     \
-        __asm__ __volatile__("movl %%ebp,%0" ::"a"(arg6));                     \
-        __asm__ __volatile__("int $49");                                       \
-    })
-
-int getprio(int pid)
+int ggetprio(int pid)
 {
-    syscall_1(2, pid);
+    printf("%d", pid);
+    //syscall_1(2, (void *)pid);
     return 0;
 }
--- shared/syscall.h
+++ shared/syscall.h
@@ -1,6 +1,6 @@
 #ifndef __SYSCALL_H__
 #define __SYSCALL_H__
 
-int getprio(int pid);
+int ggetprio(int pid);
 
 #endif
