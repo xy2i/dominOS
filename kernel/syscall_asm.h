c90acc018625254a535c4e82cf06020e3f64e7c4
Arthur Sarry <arthur.sarry@gmail.com>
2022-04-18 17:12:41 +0200

    syscall_asm: Change from void to void*

--- kernel/syscall_asm.h
+++ kernel/syscall_asm.h
@@ -1,15 +1,16 @@
 #ifndef __SYSCALL_ASM_H__
 #define __SYSCALL_ASM_H__
 
-void syscall_0(int syscall_number);
-void syscall_1(int syscall_number, void *arg1);
-void syscall_2(int syscall_number, void *arg1, void *arg2);
-void syscall_3(int syscall_number, void *arg1, void *arg2, void *arg3);
-void syscall_4(int syscall_number, void *arg1, void *arg2, void *arg3,
-               void *arg4);
-void syscall_5(int syscall_number, void *arg1, void *arg2, void *arg3,
-               void *arg4, void *arg5);
-void syscall_6(int syscall_number, void *arg1, void *arg2, void *arg3,
-               void *arg4, void *arg5, void *arg6);
+//You must cast the return value of syscall_n to proper type
+void *syscall_0(int syscall_number);
+void *syscall_1(int syscall_number, void *arg1);
+void *syscall_2(int syscall_number, void *arg1, void *arg2);
+void *syscall_3(int syscall_number, void *arg1, void *arg2, void *arg3);
+void *syscall_4(int syscall_number, void *arg1, void *arg2, void *arg3,
+                void *arg4);
+void *syscall_5(int syscall_number, void *arg1, void *arg2, void *arg3,
+                void *arg4, void *arg5);
+void *syscall_6(int syscall_number, void *arg1, void *arg2, void *arg3,
+                void *arg4, void *arg5, void *arg6);
 
 #endif
