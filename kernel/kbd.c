6d6233b855e8b2e7add64bb9620be48f21cffe47
Tom Mendez <Tom.Mendez-Porcel@grenoble-inp.org>
2022-04-20 23:56:05 +0200

    tentative implem keyboard data

--- kernel/kbd.c
+++ kernel/kbd.c
@@ -4,11 +4,24 @@
 /* You have to implement this function. It is called by do_scancode with
 a string that is the translation of the scancodes into characters. */
 void keyboard_data(char *str){
-    (void)str;
+    //(void)str;
+    
+    //cons_read doit être appelée quelque part au préalable
 
-    // int len = 0;
-    // (recherche de len)
-    // cons_write(str, len(str)) s le caractère est writeable
+    int i = 0;
+    while((str[i] != '\0') && (i<100)){
+        if(str[i]== 13){  // Entrée
+            break;
+        }else if(str[i]== 127){
+            i--;
+        }else{
+            keyboard_buffer[i] = str[i];
+            i++;
+        }
+    }
+
+    // if echo
+    // cons_write(keyboard_buffer, i)
 }
 
 /* You may implement this function to keep keyboard LEDs in sync with the
