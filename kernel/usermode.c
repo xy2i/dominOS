2d9628684a553550a1e980d0fe353c708cb28021
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-04-25 12:40:55 +0200

    usermode: remove old adrien usermode.c/h

--- kernel/usermode.c
+++
@@ -1,7 +0,0 @@
-#include <stddef.h>
-#include <stdint.h>
-#include <string.h>
-#include "parameters.h"
-#include "start.h"
-#include "task.h"
-#include "userspace_apps.h"
--- kernel/usermode.h
+++ kernel/usermode.h
@@ -1,6 +1,6 @@
 #ifndef __USERMODE_H__
 #define __USERMODE_H__
 
-void goto_user_mode(int (*func_ptr)(void *), unsigned long esp);
+void goto_user_mode(uint32_t esp);
 
 #endif
