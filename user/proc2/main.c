b6bcd211c02d2cf3cbf5d369c1630999b21499cc
Arthur Sarry <arthur.sarry@gmail.com>
2022-04-27 11:31:18 +0200

    user/proc2/main: call proc1

--- user/proc2/main.c
+++ user/proc2/main.c
@@ -1,15 +1,13 @@
-
+#define WITH_SEM
+#include "../tests/lib/sysapi.h"
 
 int main()
 {
-    // ggetprio(0);
-    __asm__("mov $2, %eax");
-    __asm__("mov $2, %eax");
-    __asm__("mov $2, %eax");
-    __asm__("mov $2, %eax");
-    __asm__("mov $2, %eax");
-    while (1) {
-        return -1;
+    for (int i = 0; i < 20; i++) {
+        int pid = start("proc1", 0x401, 1, NULL);
+        int retval = 0;
+        waitpid(pid, &retval);
+        cons_write("hello", sizeof("hello"));
     }
     return 0;
 }
