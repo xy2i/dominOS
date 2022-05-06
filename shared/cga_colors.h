add4228fec12e55214962b46fdf5c6d21bbf4a19
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-05-06 09:26:27 +0200

    cga: split header

---
+++ shared/cga_colors.h
@@ -0,0 +1,39 @@
+
+#ifndef __CGA_COLORS_H__
+#define __CGA_COLORS_H__
+
+/*
+ * CGA colors definition.
+ */
+
+#define BLINK 0X80
+
+#define BLACK_FG 0x00
+#define BLUE_FG 0x01
+#define GREEN_FG 0x02
+#define CYAN_FG 0x03
+#define RED_FG 0x04
+#define MAGENTA_FG 0x05
+#define BROWN_FG 0x06
+#define GREY_FG 0x07
+#define DARK_GREY_FG 0x08
+#define LIGHT_BLUE_FG 0x09
+#define LIGHT_GREEN_FG 0x0A
+#define LIGHT_CYAN_FG 0x0B
+#define LIGHT_RED_FG 0x0C
+#define LIGHT_MAGENTA_FG 0x0D
+#define LIGHT_YELLOW_FG 0X0E
+#define LIGHT_WHITE_FG 0x0F
+
+#define BLACK_BG 0X00
+#define BLUE_BG 0x10
+#define GREEN_BG 0x20
+#define CYAN_BG 0x30
+#define RED_BG 0x40
+#define MAGENTA_BG 0x50
+#define BROWN_BG 0x60
+#define GREY_BG 0x70
+
+#define DEFAULT LIGHT_WHITE_FG
+
+#endif //__CGA_COLORS_H__
