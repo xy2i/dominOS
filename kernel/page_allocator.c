3963cabb7cbf026733efb2762b99931c3ec54cb0
Valentin Laclautre <valentin-laclautre@hotmail.fr>
2022-05-06 17:20:26 +0200

    Paging: make a unmap function for virtual address

--- kernel/page_allocator.c
+++ kernel/page_allocator.c
@@ -58,93 +58,98 @@ void alloc_block()
     }
 }
 
-uint32_t puiss2(unsigned long size) {
-    uint32_t p=0;
-    size = size - 1; // allocation start in 0
-    while(size) {  // get the largest bit
-	p++;
-	size >>= 1;
+uint32_t puiss2(unsigned long size)
+{
+    uint32_t p = 0;
+    size       = size - 1; // allocation start in 0
+    while (size) { // get the largest bit
+        p++;
+        size >>= 1;
     }
-    if ((int) size > (1 << p))
-	p++;
+    if ((int)size > (1 << p))
+        p++;
     return p;
 }
 
-
-void *alloc_physical_page(int nb_pages) {
+void *alloc_physical_page(int nb_pages)
+{
     init_alloc();
     assert(nb_pages > 0);
     assert(nb_pages < NB_PAGES_ALLOC);
 
     //size we want to allocate
-    uint32_t size = nb_pages<<SHIFT;
+    uint32_t size = nb_pages << SHIFT;
 
     //index in the map (the index 0 correspond to a size of 4096 that's why we do -12)
     uint32_t index = puiss2(size) - SHIFT;
 
     //find the first index where there is a block
     uint32_t scan_index = index;
-    while(area.map[scan_index] == NULL) {
-	scan_index++;
+    while (area.map[scan_index] == NULL) {
+        scan_index++;
 
-	//reallocate memory if needed
-	if(scan_index == MAP_SIZE - 1 && area.map[scan_index] == NULL) {
-	    alloc_block();
-	    *((void **) area.map[scan_index]) = NULL;
-	}
+        //reallocate memory if needed
+        if (scan_index == MAP_SIZE - 1 && area.map[scan_index] == NULL) {
+            alloc_block();
+            *((void **)area.map[scan_index]) = NULL;
+        }
     }
 
     // Split blocks until to have the needed size
-    while(scan_index != index) {
-	void *curr_block = area.map[scan_index];
-	area.map[scan_index] = *((void **) curr_block);
-	scan_index--;
-	area.map[scan_index] = curr_block;
+    while (scan_index != index) {
+        void *curr_block     = area.map[scan_index];
+        area.map[scan_index] = *((void **)curr_block);
+        scan_index--;
+        area.map[scan_index] = curr_block;
 
-	//Buddy calculation
-	uint32_t buddy_block = (uint32_t) curr_block ^ (1<<(scan_index+SHIFT));
-	*((void **) area.map[scan_index]) = (void *) buddy_block;
-	*((void **) buddy_block) = NULL;
+        //Buddy calculation
+        uint32_t buddy_block =
+            (uint32_t)curr_block ^ (1 << (scan_index + SHIFT));
+        *((void **)area.map[scan_index]) = (void *)buddy_block;
+        *((void **)buddy_block)          = NULL;
     }
 
-    void *ptr = area.map[index];
-    area.map[index] = *((void **) ptr);
+    void *ptr       = area.map[index];
+    area.map[index] = *((void **)ptr);
 
     return ptr;
 }
 
-void free_physical_page(void *physical_page, int nb_pages) {
+void free_physical_page(void *physical_page, int nb_pages)
+{
     //size allocated
-    uint32_t size = nb_pages<<SHIFT;
-    uint32_t index = puiss2(size) - SHIFT;
-    uint32_t buddy_address = ((uint32_t) physical_page) ^ (1<<(index+SHIFT));
+    uint32_t size          = nb_pages << SHIFT;
+    uint32_t index         = puiss2(size) - SHIFT;
+    uint32_t buddy_address = ((uint32_t)physical_page) ^ (1 << (index + SHIFT));
 
-    void *scan_ptr = area.map[index];
+    void *scan_ptr     = area.map[index];
     void *previous_ptr = NULL;
-    while(scan_ptr != NULL) {
-	if((uint32_t) scan_ptr == buddy_address) {
-	    // Buddy addr is found : regroup + init to do the same on index + 1
-	    if(previous_ptr != NULL) {
-		*((void **) previous_ptr) = *((void **) scan_ptr);
-	    } else {
-		area.map[index] = *((void **) scan_ptr);
-	    }
-	    index++;
-	    if(index == MAP_SIZE) {
-		break;
-	    }
-	    physical_page = ((uint32_t) physical_page < buddy_address) ? physical_page : (void *) buddy_address;
-	    scan_ptr = area.map[index];
-	    buddy_address = ((uint32_t) physical_page) ^ (1<<(index+SHIFT));
-	    previous_ptr = NULL;
-	    continue;
-	}
-	previous_ptr = scan_ptr;
-	if(scan_ptr != NULL) {
-	    scan_ptr = *((void **) scan_ptr);
-	}
+    while (scan_ptr != NULL) {
+        if ((uint32_t)scan_ptr == buddy_address) {
+            // Buddy addr is found : regroup + init to do the same on index + 1
+            if (previous_ptr != NULL) {
+                *((void **)previous_ptr) = *((void **)scan_ptr);
+            } else {
+                area.map[index] = *((void **)scan_ptr);
+            }
+            index++;
+            if (index == MAP_SIZE) {
+                break;
+            }
+            physical_page = ((uint32_t)physical_page < buddy_address) ?
+                                physical_page :
+                                (void *)buddy_address;
+            scan_ptr      = area.map[index];
+            buddy_address = ((uint32_t)physical_page) ^ (1 << (index + SHIFT));
+            previous_ptr  = NULL;
+            continue;
+        }
+        previous_ptr = scan_ptr;
+        if (scan_ptr != NULL) {
+            scan_ptr = *((void **)scan_ptr);
+        }
     }
-    void *next_ptr = area.map[index];
-    area.map[index] = physical_page;
-    *((void **) area.map[index]) = next_ptr;
+    void *next_ptr              = area.map[index];
+    area.map[index]             = physical_page;
+    *((void **)area.map[index]) = next_ptr;
 }
