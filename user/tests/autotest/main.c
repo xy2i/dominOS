5edcf44ca1e3555b0a22ebffb233005b764ec57c
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-05-07 11:01:01 +0200

    autotest: continue even when a test fail

--- user/tests/autotest/main.c
+++ user/tests/autotest/main.c
@@ -5,45 +5,32 @@
 
 #include "sysapi.h"
 
-#define TESTS_NUMBER    23
+#define TESTS_NUMBER 23
 
 const char *tests[TESTS_NUMBER] = {
-        "test0",
-        "test1",
-        "test2",
-        "test3",
-        "test4",
-        "test5",
-        "test6",
-        "test7",
-        "test8",
-        "test9",
-        "test10",
-        "test11",
-        "test12",
-        "test13",
-        "test14",
-        "test15",
-        "test16",
-        "test17",
-        "test18",
-        "test19",
-        "test20",
-        "test21",
-        "test22",
+    "test0",  "test1",  "test2",  "test3",  "test4",  "test5",
+    "test6",  "test7",  "test8",  "test9",  "test10", "test11",
+    "test12", "test13", "test14", "test15", "test16", "test17",
+    "test18", "test19", "test20", "test21", "test22",
 };
+
+extern void change_color(unsigned char color);
+#define RED_FG 0x04
+#define LIGHT_WHITE_FG 0x0F
+#define DEFAULT LIGHT_WHITE_FG
 
 int main(void)
 {
-        int i;
-        int pid;
-        int ret;
+    int i;
+    int pid;
+    int ret;
 
-        for (i = 0; i < TESTS_NUMBER; i++) {
-                printf("Test %s : ", tests[i]);
-                pid = start(tests[i], 4000, 128, NULL);
-                waitpid(pid, &ret);
-                assert(ret == 0);
+    for (i = 0; i < TESTS_NUMBER; i++) {
+        printf("Test %s : ", tests[i]);
+        pid = start(tests[i], 4000, 128, NULL);
+        waitpid(pid, &ret);
+        if (ret != 0) {
+            assert(ret != 0);
         }
+    }
 }
-
