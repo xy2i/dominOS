1e9969230ddcf55e9d05092c459b0a0d6ab149a0
Arthur Sarry <arthur.sarry@gmail.com>
2022-05-26 18:53:30 +0200

    fat32: Update BIOS Parameter Block structure

--- kernel/fat32.c
+++ kernel/fat32.c
@@ -3,20 +3,22 @@
 
 // https://wiki.osdev.org/FAT
 struct BPB {
+    // Original BPB
     uint8_t jmp_boot[3];
     char oem_name[8];
     uint16_t bytes_per_sector;
     uint8_t sector_per_cluster;
     uint16_t nbr_reserved_sectors;
     uint8_t nbr_fats;
-    uint16_t root_entry_count;
-    uint16_t total_sectors_in_volume;
+    uint16_t nbr_root_entry;
+    uint16_t nbr_sectors_in_volume;
     uint8_t media;
     uint16_t nbr_sectors_per_fat;
     uint16_t nbr_sectors_per_track;
     uint16_t nbr_heads;
     uint32_t nbr_hidden_sectors;
     uint32_t nbr_large_sectors;
+    // FAT32 extension
     uint32_t nbr_sectors_per_fat32;
     uint32_t extension_flags;
     uint16_t fat_version;
