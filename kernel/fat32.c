793d11f01277b500efd21838e93002beb0856715
Hugo Elhaj-Lahsen <hugo@xy2.dev>
2022-05-26 22:14:40 +0200

    fat32: Rest of structures

--- kernel/fat32.c
+++ kernel/fat32.c
@@ -1,3 +1,10 @@
+/**
+ * FAT32 driver.
+ *
+ * https://wiki.osdev.org/FAT
+ * http://www.osdever.net./documents/fatgen103.pdf - MS doc
+ */
+
 #include "fat32.h"
 #include "stdint.h"
 
@@ -45,3 +52,80 @@ struct FSInfo {
     uint8_t reserved_2[12];
     uint32_t trail_signature;
 } __attribute__((__packed__));
+
+struct DirEntry {
+    uint8_t filename[11];
+
+    union {
+        struct {
+            uint8_t read_only : 1;
+            uint8_t hidden : 1;
+            uint8_t system : 1;
+            uint8_t volume_id : 1;
+            uint8_t directory : 1;
+            uint8_t archive : 1;
+            uint8_t reserved1 : 2;
+        };
+        uint8_t attr;
+    };
+
+    uint8_t win_nt_reserved;
+
+    // Values in tenths of ms, from 0-200
+    uint8_t created_ticks;
+    // Has a 2 second granularity, not 1 second!
+    uint16_t created_second : 5;
+    uint16_t created_minute : 6;
+    uint16_t created_hour : 5;
+    uint16_t created_day : 5;
+    uint16_t created_month : 4;
+    uint16_t created_year : 7;
+
+    uint16_t accessed_day : 5;
+    uint16_t accessed_month : 4;
+    uint16_t accessed_year : 7;
+
+    uint16_t cluster_hi;
+
+    uint16_t modified_second : 5;
+    uint16_t modified_minute : 6;
+    uint16_t modified_hour : 5;
+
+    uint16_t modified_day : 5;
+    uint16_t modified_month : 4;
+    uint16_t modified_year : 7;
+
+    uint16_t cluster_lo;
+
+    uint32_t size;
+} __attribute__((__packed__));
+
+#define LAST_LONG_ENTRY 0x40
+// https://en.wikipedia.org/wiki/Design_of_the_FAT_file_system#VFAT_long_file_names
+struct LongNameEntry {
+    // Mask with LAST_LONG_ENTRY to know if this is the last entry in set
+    uint8_t order;
+    uint16_t filename_lo[5];
+    // Must be Attr.LONG_NAME
+    uint8_t attr;
+    uint8_t type; // 0 = long entry
+    uint8_t checksum_of_short_name;
+    uint16_t filename_mid[6];
+    uint16_t zero;
+    uint16_t filename_hi[2];
+} __attribute__((__packed__));
+
+// For DIR_Attr, reversed since little endian
+enum Attr {
+    READ_ONLY = 0x80,
+    HIDDEN = 0x40,
+    SYSTEM = 0x20,
+    VOLUME_ID = 0x10,
+    DIRECTORY = 0x8,
+    ARCHIVE = 0x4,
+    LONG_NAME = READ_ONLY | HIDDEN | SYSTEM | VOLUME_ID,
+    LONG_NAME_MASK =
+        READ_ONLY | HIDDEN | SYSTEM | VOLUME_ID | DIRECTORY | ARCHIVE,
+};
+
+int init_fat();
